3aaa908db7911f9cef4e7fa143f3fe829ab30801
Juan Bulla <juanbulla9@gmail.com>
2024-03-03 13:40:16 -0300

    wip

--- microphone-processor.js
+++ microphone-processor.js
@@ -1,18 +1,17 @@
 // microphone-processor.js
 class MicrophoneProcessor extends AudioWorkletProcessor {
-  process(inputs, outputs, parameters) {
-    const input = inputs[0];
-    const output = outputs[0];
+    process(inputs, outputs, parameters) {
+        const input = inputs[0];
+        const output = outputs[0];
 
-    for (let channel = 0; channel < output.length; ++channel) {
-      for (let i = 0; i < input[channel].length; ++i) {
-        output[channel][i] = input[channel][i];
-      }
+        if (!input || input.length === 0 || !input[0]) {
+            return true;
+        }
+
+        const microphoneOutputBuffer = input[0];
+        this.port.postMessage(microphoneOutputBuffer);
+        return true;
     }
-
-    this.port.postMessage(input[0]);
-    return true;
-  }
 }
 
 registerProcessor('microphone-processor', MicrophoneProcessor);
